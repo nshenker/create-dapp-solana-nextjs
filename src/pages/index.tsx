19480354575fb24bc1155e8d44572eddc53c13ef
NFHustle <nfhustle@outlook.com>
2022-09-12 01:55:23 -0400

    Augie

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { HomeView } from "../views";
+import { GallerView } from "../views";
 
 const Home: NextPage = (props) => {
   return (
@@ -12,7 +12,7 @@ const Home: NextPage = (props) => {
           content="This site will fly high 🦤"
         />
       </Head>
-      <HomeView />
+      <GalleryView />
     </div>
   );
 };
