9726bc3ab5629f49bd4deaad155f750bb7b29173
NFHustle <nfhustle@outlook.com>
2022-09-12 01:58:05 -0400

    Augie

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { GallerView } from "../views";
+import { GalleryView } from "../views";
 
 const Home: NextPage = (props) => {
   return (
