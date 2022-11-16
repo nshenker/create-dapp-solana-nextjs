d5c151a45b6e091eec63e2e7fec39cd07f616800
NFHustle <nfhustle@outlook.com>
2022-11-15 21:24:39 -0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,7 @@ import "../styles/App.css";
 // set custom RPC server endpoint for the final website
 // const endpoint = "https://explorer-api.devnet.solana.com";
 // const endpoint = "http://127.0.0.1:8899";
-const endpoint = "https://ssc-dao.genesysgo.net";
+const endpoint = "https://green-bold-seed.solana-mainnet.discover.quiknode.pro/0027f801211bf821310389a08c7c36593580a080/";
 
 const WalletProvider = dynamic(
   () => import("../contexts/ClientWalletProvider"),
